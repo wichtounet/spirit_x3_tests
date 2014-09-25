5b0df51676bfb0363815684c48a7313b6c98843e
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-09-25 21:19:09 +0200

    Not nice 3 problem

--- Makefile
+++ Makefile
@@ -50,6 +50,10 @@ not_nice_2: src/not_nice_2.cpp
 	$(CC) $(CXX_FLAGS) -o not_nice_2.o -c src/not_nice_2.cpp
 	$(LD) $(LD_FLAGS) -o not_nice_2 not_nice_2.o
 
+not_nice_3: src/not_nice_3.cpp
+	$(CC) $(CXX_FLAGS) -o not_nice_3.o -c src/not_nice_3.cpp
+	$(LD) $(LD_FLAGS) -o not_nice_3 not_nice_3.o
+
 monster: src/monster.cpp
 	$(CC) -fno-rtti -O2 -ftemplate-depth-2048 $(CXX_FLAGS) -o monster.o -c src/monster.cpp
 	$(LD) $(LD_FLAGS) -o monster monster.o
@@ -65,4 +69,6 @@ clean:
 	rm -rf problem_7
 	rm -rf problem_8
 	rm -rf not_nice_1
+	rm -rf not_nice_2
+	rm -rf not_nice_3
 	rm -rf monster
---
+++ src/not_nice_3.cpp
@@ -0,0 +1,56 @@
+#include <boost/spirit/home/x3.hpp>
+#include <boost/spirit/home/x3/support/ast/variant.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+
+namespace x3 = boost::spirit::x3;
+
+namespace x3_ast {
+
+typedef x3::variant<int, char, double> value;
+
+} //end of x3_ast namespace
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Woverloaded-shift-op-parentheses"
+
+namespace x3_grammar {
+
+    struct value_class {};
+    struct primary_value_class {};
+
+    x3::rule<value_class, x3_ast::value> const value("value");
+    x3::rule<primary_value_class, x3_ast::value> const primary_value("value");
+
+    auto const primary_value_def = 
+            x3::int_ 
+        |   x3::char_ 
+        |   x3::double_;
+
+    auto const value_def = 
+            primary_value
+        |   '(' > primary_value > ')'
+        ;
+
+    BOOST_SPIRIT_DEFINE(
+        value = value_def,
+        primary_value = primary_value_def
+    );
+
+    auto parser = value;
+
+} // end of grammar namespace
+
+#pragma clang diagnostic pop
+
+int main(int argc, char** argv){
+    std::string file_contents = "fake";
+
+    auto& parser = x3_grammar::parser;
+
+    x3_ast::value result;
+    boost::spirit::x3::ascii::space_type space;
+
+    x3::phrase_parse(file_contents.begin(), file_contents.end(), parser, space, result);
+
+    return 0;
+}
